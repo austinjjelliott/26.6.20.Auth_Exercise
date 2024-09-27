f9dde978cd77fd2e523702dec456493afa52ee89
Austin E <austinjjelliott@gmail.com>
2024-09-27 13:16:07 -0700

    Finished Part8

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, session, flash
+from flask import Flask, render_template, redirect, session, flash, url_for
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db, User
-from forms import UserForm, LoginForm
+from models import connect_db, db, User, Feedback
+from forms import UserForm, LoginForm, FeedbackForm
 from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
@@ -68,12 +68,91 @@ def show_user(username):
         flash('Please Login To View')
         return redirect('/')
     user = User.query.get_or_404(session['user_id'])
-    return render_template('user_homepage.html', user = user)
+    feedback = user.feedback
+    form = FeedbackForm()
+    return render_template('user_homepage.html', user = user, feedback = feedback, form = form)
 
 
+@app.route('/users/<username>/delete', methods = ["POST"])
+def delete_user(username):
+    user = User.query.get_or_404(username)
+    feedback = user.feedback
+    if session['user_id'] != user.username:
+        flash('You need to login first!')
+        return redirect('/login')
+    if user.username == session['user_id']:
+        db.session.delete(user)
+        db.session.commit()
+        session.clear()  # Clear session to log the user out
+        flash('Account deleted', 'danger')
+        return redirect('/register')
+    return render_template('user_homepage.html', user = user, feedback = feedback)
 
 @app.route('/logout')
 def logout_user():
     session.pop('user_id')
     flash('You Are Now Logged Out', 'danger')
     return redirect('/')
+
+
+@app.route('/feedback/<int:id>/delete', methods = ['POST'])
+def delete_feedback(id):
+    if 'user_id' not in session:
+        flash('Please login first!', 'danger')
+        return redirect('/')
+    user = User.query.get_or_404(session['user_id'])
+    feedback = Feedback.query.get_or_404(id)
+    if feedback.username == session['user_id']:
+        db.session.delete(feedback)
+        db.session.commit()
+        flash('Feedback Deleted!', 'success')
+        return redirect(url_for('show_user', username = user.username))
+    
+@app.route('/users/<username>/feedback/add', methods = ['GET', 'POST'])
+def add_feedback(username):
+    user = User.query.get_or_404(username)
+    if 'user_id' not in session or session['user_id'] != username:
+        flash('You must be logged in to add feedback', 'danger')
+        return redirect('/')
+    form = FeedbackForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        new_feed = Feedback(title = title, content = content, username = username)
+        db.session.add(new_feed)
+        db.session.commit()
+        flash('Feedback Submitted', 'success')
+        return redirect(url_for('show_user', username = username))
+    return render_template('add_feedback.html', user = user, form = form)
+
+@app.route('/feedback/<int:id>/update', methods = ['GET', 'POST'])
+def update_feedback(id):
+    user = User.query.get_or_404(session['user_id'])
+    feedback = Feedback.query.get_or_404(id)
+    form = FeedbackForm(obj = feedback)
+    if 'user_id' not in session:
+        flash('Please login first!', 'danger')
+        return redirect('/')
+    if feedback.username == session['user_id']:
+        if form.validate_on_submit():
+            feedback.title = form.title.data
+            feedback.content = form.content.data
+            db.session.commit()
+            flash('Feedback has been updated!', 'success')
+            return redirect(url_for('show_user', username = session['user_id']))
+    return render_template('update_feedback.html', user = user, feedback = feedback, form = form)
+
+
+
+
+
+# **GET */feedback/<feedback-id>/update :*** Display a form to edit feedback —
+# Make sure that only the user who has 
+# written that feedback can see this form **
+
+# **POST */feedback/<feedback-id>/update :*** Update a specific piece of feedback
+# and redirect to /users/<username> — **Make sure that only the user who has 
+# written that feedback can update it.**
+
+
+
--- forms.py
+++ forms.py
@@ -14,11 +14,7 @@ class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(), Length(max=20)])
     password = PasswordField("Password", validators=[InputRequired()])
 
-    
-# **GET */login :*** Show a form that when submitted will login a user. 
-# This form should accept a username and a password. Make sure you are using 
-# WTForms and that your password input hides the characters that the user is 
-# typing!
 
-# **POST */login :*** Process the login form, ensuring the user is authenticated 
-# and going to ***/secret*** if so.
+class FeedbackForm(FlaskForm):
+    title = StringField("Title", validators=[InputRequired(), Length(max = 100)])
+    content = StringField("Content", validators=[InputRequired()])
