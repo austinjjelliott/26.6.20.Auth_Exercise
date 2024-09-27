5b70774867990fb38ac4d3694d3d1985d4340d76
Austin E <austinjjelliott@gmail.com>
2024-09-27 10:59:15 -0700

    Completed Part 7

--- models.py
+++ models.py
@@ -37,3 +37,13 @@ class User(db.Model):
             return u 
         else:
             return False
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.String(100), nullable = False)
+    content = db.Column(db.String, nullable = False)
+    username = db.Column(db.String, db.ForeignKey('users.username')) 
+
+    user = db.relationship('User', backref = 'feedback')   
